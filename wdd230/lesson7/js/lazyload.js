1387b7b10205ee7a4224e7e0f57f1ce30b5b949f
Zach Potter <pot20003@byui.edu>
2021-02-19 00:03:02 -0700

    trying to fix lazy load

--- wdd230/lesson7/js/lazyload.js
+++ wdd230/lesson7/js/lazyload.js
@@ -1,32 +1,42 @@
-const imagesToLoad = document.querySelectorll("img[data-src]");
+let imagesToLoad = document.querySelectorAll("data-src");
 
-function preloadImage(img) {
-    const src = img.getAttribute("data-src");
-    if(!src) {
-        return;
-    }
-    img.src = src;
+function preLoadImage (img) {
+const src= imagesToLoad.getAttribute("data-src");
+if (!src) {
+    return;
+}
+img.src = src
+
 }
 const imgOptions = {
-    threshold = 0,
+    threshold = 1,
     rootMargin: "0px 0px 50px 0px"
-
 };
-
-    const imgObserver = new IntersectionObserver((entries,
-        imgObserver) => {
-        entries.forEach(entry => {
-            if (!entry.isIntersecting) {
-                return;
-            }   else {
-                    preloadImage(entry.target);
-                    imgObserver.unobserve(entry.target);
+const loadImages = (image) => {
+  image.setAttribute('src', image.getAttribute('data-src'));
+  image.onload = () => {
+    image.removeAttribute('data-src');
+  };
+};
+if ('IntersectionObserver' in window){
+const imgObserver = new IntersectionObserver((entries,
+    imgObserver) => {
+    entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+        return;
+    }   else {
+            preLoadImage(entry.target);
+            imgObserver.unobserve(entry.target);
                 }
         })
 }, imgOptions);
 
-images.forEach(image => {
-    imgObserver.observe(image);
-
-
+imagesToLoad.forEach((img) => {
+    imgObserver.observe(img);
 });
+}
+else {
+ imagesToLoad.forEach((img) => {
+ loadImages(img);
+});
+}
