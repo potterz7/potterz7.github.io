115e874815f9e8cf576cc424682a8cd8a3598872
Zach Potter <pot20003@byui.edu>
2021-02-20 21:47:32 -0700

    math.round

--- wdd230/lesson6/js/lesson6.js
+++ wdd230/lesson6/js/lesson6.js
@@ -48,7 +48,7 @@ let wind_speed = 5;
 document.getElementById('windspeed').innerHTML= wind_speed;
 
 var a = Math.pow(wind_speed,.16);
-let wind_chill = math.round(35.74 + 0.6215 * temperature -35.75* a + .4275 * temperature * a);
+let wind_chill = Math.round(35.74 + 0.6215 * temperature -35.75* a + .4275 * temperature * a);
 
 
 document.getElementById('windchill').innerHTML = wind_chill;
