4e2eda7f21314dd2486e12bd766d5bf81bcb7381
Zach Potter <pot20003@byui.edu>
2021-03-05 00:16:28 -0700

    prophets assignment

---
+++ wdd230/lesson9/js/prophets.js
@@ -0,0 +1,26 @@
+const requestURL = 'https://byui-cit230.github.io/canvas-referenced/latter-day-prophets.json';
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject);  // temporary checking for valid response and data parsing
+    const prophets = jsonObject['prophets'];
+    const cards = document.querySelector('.cards');
+  
+    prophets.forEach(prophet => {
+        let card = document.createElement('section');
+        let h2 = document.createElement('h2');
+        let p = document.createElement('p');
+        let img = document.createElement('img');
+    // use tempplate literals
+        h2.innerHTML = `${prophet.name} ${prophet.lastname}`;
+        p.innerHTML = `Date of Birth: ${prophet.birthdate} <br> Place of Birth: ${prophet.birthplace} `
+        img.setAttribute('src', prophet.imageurl)
+        card.append(h2);
+        card.append(p);
+        card.append(img);
+        cards.append(card);
+    });
+  });
