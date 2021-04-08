0ae0e66451f722a2149d0c50c978460cb19bbe04
Zach Potter <pot20003@byui.edu>
2021-04-08 00:51:38 -0600

    changes made

--- wdd230/ChamberOfCommerce/js/Chamber.js
+++ wdd230/ChamberOfCommerce/js/Chamber.js
@@ -29,27 +29,23 @@ document.getElementById('currentdate').textContent = currentdate;
 
 
 
-const movie = document.getElementById('freefinancialclass');
-if (d.getDay() ==3) {
-    movie.style.display = 'block'
+const finance = document.getElementById('freefinancialclass');
+if (d.getDay() ==4) {
+    finance.style.display = 'block'
 }
     else {
-        movie.style.display = 'none'
+        finance.style.display = 'none'
 };
 
 function toggleMenu() {
     document.getElementById("primaryNav").classList.toggle("hide");
 };
 
-function adjustRating(rating) {
-    document.getElementById("ratingvalue").innerHTML = rating;
-}
-
 function thanks(){
     var thankyou = "https://potterz7.github.io/wdd230/ChamberOfCommerce/thankyou.html";
     window.location.href = thankyou;
+};
+function thankyou(){
+    var thanks = "https://potterz7.github.io/wdd230/ChamberOfCommerce/comments.html";
+    window.location.href = thanks;
 }
-
-
-
-
